7db724dab045e1efd3dde1861030121698ae0e29
Sergiu Botnaruc <botnaruc@yahoo.com>
2021-12-08 12:10:54 +0200

    Initial commit

---
+++ src/main/java/org/stabila/core/db/RevokingDatabase.java
@@ -0,0 +1,41 @@
+package org.stabila.core.db;
+
+import org.stabila.core.db2.common.IRevokingDB;
+import org.stabila.core.db2.core.ISession;
+import org.stabila.core.exception.RevokingStoreIllegalStateException;
+
+public interface RevokingDatabase {
+
+  ISession buildSession();
+
+  ISession buildSession(boolean forceEnable);
+
+  void setMode(boolean mode);
+
+  void add(IRevokingDB revokingDB);
+
+  void merge() throws RevokingStoreIllegalStateException;
+
+  void revoke() throws RevokingStoreIllegalStateException;
+
+  void commit() throws RevokingStoreIllegalStateException;
+
+  void pop() throws RevokingStoreIllegalStateException;
+
+  void fastPop() throws RevokingStoreIllegalStateException;
+
+  void enable();
+
+  int size();
+
+  void check();
+
+  void setMaxSize(int maxSize);
+
+  void disable();
+
+  void setMaxFlushCount(int maxFlushCount);
+
+  void shutdown();
+
+}
---
+++ src/main/java/org/stabila/core/db2/common/Instance.java
@@ -0,0 +1,5 @@
+package org.stabila.core.db2.common;
+
+public interface Instance<T> {
+  T newInstance();
+}
